27d877dbe9055c12ccaacc6ca9bdd46df68d7bb1
Luke Redmore <luke.redmore@gmail.com>
2019-08-14 17:24:23 -0400

    Improve Events Data Parser

--- CSBC/Screens/Calendar/Model/EventsDataParser.swift
+++ CSBC/Screens/Calendar/Model/EventsDataParser.swift
@@ -15,108 +15,79 @@ class EventsDataParser {
     private let monthDict = ["jan":"01","feb":"02","mar":"03","apr":"04","may":"05","jun":"06","jul":"07","aug":"08","sep":"09","oct":"10","nov":"11","dec":"12"]
     var eventsModelArray : [EventsModel?] = []
     var eventsModelArrayFiltered : [EventsModel?] = []
-   
+    
     
     func parseHTMLForEvents (html : String) {
-        do {
-            var infoOrganizedByEvent : [String] = []
-            let doc = try SwiftSoup.parse(html)
-            let allPInfo = try doc.select("p").array()
-            
-            for i in allPInfo.indices {
-                let pClass = try allPInfo[i].attr("class")
-                if pClass == "desc_trig_outter" {
-                    let text = try allPInfo[i].html()
-                    infoOrganizedByEvent.append(text)
-                }
+        var infoOrganizedByEvent : [String] = []
+        
+        guard let allPInfo = try? SwiftSoup.parse(html).select("p").array() else { return }
+        for i in allPInfo.indices {
+            if let pClass = try? allPInfo[i].attr("class"), pClass == "desc_trig_outter" {
+                guard let text = try? allPInfo[i].html() else { return }
+                infoOrganizedByEvent.append(text)
             }
+        }
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM/dd"
+        let fmt = DateFormatter()
+        fmt.dateFormat = "MMMM dd"
+        for n in infoOrganizedByEvent.indices {
+            var dateString, day, month, time, event, schools, eventMonth: String?
             
-            let formatter = DateFormatter()
-            formatter.dateFormat = "MM/dd"
-            let fmt = DateFormatter()
-            fmt.dateFormat = "MMMM dd"
-            for n in infoOrganizedByEvent.indices {
-                var dateString = String()
-                var day = String()
-                var month = String()
-                var time = String()
-                var event = String()
-                var schools = String()
-                var eventMonth = String()
-                
-                let eventDoc = try SwiftSoup.parse(infoOrganizedByEvent[n])
-                let titleInfo = try eventDoc.select("span").array()
-                for i in titleInfo.indices {
-                    let tag = try titleInfo[i].attr("itemprop")
-                    if tag == "name" {
-                        event = try titleInfo[i].text()
-                    }
+            guard let eventDoc = try? SwiftSoup.parse(infoOrganizedByEvent[n]) else { return }
+            guard let titleInfo = try? eventDoc.select("span").array() else { return }
+            for i in titleInfo.indices {
+                if let tag = try? titleInfo[i].attr("itemprop"), tag == "name" {
+                    event = try! titleInfo[i].text()
                 }
                 
-                let timeInfo = try eventDoc.select("em").array()
+                guard let timeInfo = try? eventDoc.select("em").array() else { return }
                 for i in timeInfo.indices {
-                    let classTag = try timeInfo[i].attr("class")
+                    guard let classTag = try? timeInfo[i].attr("class") else { return }
                     if classTag == "date" {
-                        day = try timeInfo[i].text()
+                        day = try! timeInfo[i].text()
                     }
                     else if classTag == "month" {
-                        let text = try timeInfo[i].text()
-                        if monthDict[text] == nil {
-                            eventMonth = monthDict[text]!
-                        } else {
-                            eventMonth = text
-                        }
+                        let text = try! timeInfo[i].text()
+                        eventMonth = monthDict[text] == nil ? monthDict[text]! : text
                         month = text.uppercased()
-                        
                     }
                     else if classTag == "evcal_time" {
-                        var text = try timeInfo[i].text()
-                        if text.components(separatedBy: " ")[0] == "(All" {
-                            text = "All Day"
-                        }
-                        time = text
+                        guard let text = try? timeInfo[i].text().components(separatedBy: " ")[0] else { return }
+                        time = text == "(All" ? "All Day" : text
                     }
-                    let dataFilterTag = try timeInfo[i].attr("data-filter")
-                    if dataFilterTag == "event_type" {
-                        let text = try timeInfo[i].text()
-                        
-                        if schools == "" {
-                            schools = text
-                        } else {
-                            schools = "\(schools) \(text)"
-                        }
-                        
+                    if let dataFilterTag = try? timeInfo[i].attr("data-filter"), dataFilterTag == "event_type" {
+                        let text = try! timeInfo[i].text()
+                        schools = schools == nil ? text : "\(schools!) \(text)"
+                    }
+                    if eventMonth?.count == 2 {
+                        let originalDateString = "\(eventMonth!)/\(day ?? "")"
+                        let date : Date = formatter.date(from: originalDateString)!
+                        dateString = fmt.string(from: date)
+                    } else {
+                        eventMonth = eventMonth?.capitalized
+                        dateString = "\(eventMonth!) \(day ?? "")"
                     }
                     
+                    let eventToAppend = EventsModel(
+                        date: dateString ?? "",
+                        day: day ?? "",
+                        month: month ?? "",
+                        time: time ?? "",
+                        event: event ?? "",
+                        schools: schools ?? ""
+                    )
+                    if !eventsModelArray.contains(eventToAppend) {
+                        eventsModelArray.append(eventToAppend)
+                    }
                 }
-                if eventMonth.count == 2 {
-                    let originalDateString = "\(eventMonth)/\(day)"
-                    let date : Date = formatter.date(from: originalDateString)!
-                    dateString = fmt.string(from: date)
-                } else {
-                    eventMonth = eventMonth.capitalized
-                    dateString = "\(eventMonth) \(day)"
-                }
-                
-                let eventToAppend = EventsModel(
-                    date: dateString,
-                    day: day,
-                    month: month,
-                    time: time,
-                    event: event,
-                    schools: schools
-                )
-                if !eventsModelArray.contains(eventToAppend) {
-                    eventsModelArray.append(eventToAppend)
-                }
-            }
-            if eventsModelArray.count > 1 {
-                if eventsModelArray[0] != nil {
+                if eventsModelArray.count > 1, eventsModelArray[0] != nil {
                     eventsModelArray = eventsModelArray.sorted { $0!.day < $1!.day }
                 }
             }
-            addObjectArrayToUserDefaults(eventsModelArray)
-        } catch { }
+        }
+        addObjectArrayToUserDefaults(eventsModelArray)
     }
     private func addObjectArrayToUserDefaults(_ eventsArray: [EventsModel?]) {
         print("Events array is being added to UserDefaults")
@@ -133,10 +104,9 @@ class EventsDataParser {
                 eventsModelArrayFiltered.append(eventsModelArray[modelInt])
             }
         }
-        if eventsModelArrayFiltered.count > 1 {
-            if eventsModelArrayFiltered[0] != nil {
-                eventsModelArrayFiltered = eventsModelArrayFiltered.sorted { $0!.day < $1!.day }
-            }
+        if eventsModelArrayFiltered.count > 1, eventsModelArrayFiltered[0] != nil {
+            eventsModelArrayFiltered = eventsModelArrayFiltered.sorted { $0!.day < $1!.day }
         }
     }
 }
+
