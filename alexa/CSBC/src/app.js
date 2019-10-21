dd2630f024ed4c997d4e27caa320bedfbf6fd057
Luke Redmore <luke.redmore@gmail.com>
2019-10-21 17:59:51 -0400

    Add Alexa skill logic

---
+++ alexa/CSBC/src/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// ------------------------------------------------------------------
+// APP INITIALIZATION
+// ------------------------------------------------------------------
+
+const { App } = require('jovo-framework');
+const { Alexa } = require('jovo-platform-alexa');
+const { GoogleAssistant } = require('jovo-platform-googleassistant');
+const { JovoDebugger } = require('jovo-plugin-debugger');
+const { FileDb } = require('jovo-db-filedb');
+
+const app = new App();
+
+app.use(
+    new Alexa(),
+    new GoogleAssistant(),
+    new JovoDebugger(),
+    new FileDb()
+);
+
+
+// ------------------------------------------------------------------
+// APP LOGIC
+// ------------------------------------------------------------------
+
+app.setHandler({
+    LAUNCH() {
+        return this.toIntent('HelloWorldIntent');
+    },
+
+    HelloWorldIntent() {
+        this.tell('Application launched');
+    },
+
+    GetDayOfCycleIntent() {
+        this.tell('Hey ' + this.$inputs.date.value + ', nice to meet you!');
+    },
+});
+
+module.exports.app = app;
